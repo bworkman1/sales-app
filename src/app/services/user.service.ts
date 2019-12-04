3d0341f27cfb45f4857c4c8f0774f81d83cbe9d6
bworkma2 <brianworkman43055@gmail.com>
2019-12-04 00:15:40 -0500

    Woring on post request in login form -incomplete

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,15 @@
 import {Injectable} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {Observable, throwError} from 'rxjs';
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'my-auth-token'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +17,16 @@ import {Injectable} from '@angular/core';
 export class UserService {
 
   private readonly CurrentUser;
+  private authUserUrl = 'https://reqres.in/api/login';
 
-  constructor() {
+  const;
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient) {
     this.CurrentUser = localStorage.getItem('currentUser') ? localStorage.getItem('currentUser') : '';
   }
 
@@ -16,13 +35,22 @@ export class UserService {
       return false;
     } else {
       const session = this.CurrentUser.session_id;
-      const hfc     = this.CurrentUser.hfc;
-      const store   = this.CurrentUser.store;
+      const hfc = this.CurrentUser.hfc;
+      const store = this.CurrentUser.store;
       if (session && store && hfc) {
         return true;
       }
     }
     return false;
+  }
+
+  authenticateUser(creds: FormGroup): Observable<FormGroup> {
+    return this.http.post<FormGroup>(this.authUserUrl, creds.value, httpOptions)
+      .pipe(
+        catchError(err => {
+          console.error(err.message);
+        })
+      );
   }
 
   getUser() {
